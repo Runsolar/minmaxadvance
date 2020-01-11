35497d78ca82b2443b4f6da7588b058c42a1bd5e
WDA <engineer13@outlook.com>
2020-01-11 22:43:50 +0300

    wda

--- minmaxadvance/minmaxadvance.c
+++ minmaxadvance/minmaxadvance.c
@@ -2,6 +2,7 @@
 minmaxadvance.c
 This is an implementation of the MinMax like of the sorting algorithm which mixed with the bubble sort. 
 It is just a main idea, you might it change as you like ;).
+Asymptotics O(N*LogN)-O(N^2). 
 
 Code by D.Wolf, 2020, https://github.com/Runsolar/minmaxadvance
 License: GNU GPL.
